f6aca221db0edaee8b637f6eed69a6fc1805ea54
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-09-05 16:49:12 -0300

    build: add screenshots to Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,28 +2,49 @@ import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom";
 import fetchSingleGame from "../services/fetchSingleGame.js";
 import parseDescriptionText from "../services/parseDescriptionText.js"
-import "../stylesheets/Game.css";
 import handleMoreClick from "../services/handlers.js"
+import fetchGallery from "../services/fetchGallery.js"
+import "../stylesheets/Game.css";
+
 function Game( ) {
   const [fetchedGame,setFetchedGame] = useState(null);  
   const [gameID,setGameID] = useState(useParams());
+  const [gallery,setGallery] = useState([]);
   async function fetchGame() {
     const id = gameID.id;
-    const gameData = await fetchSingleGame(id); // armar un fetchSingleGame y reemplazar fetchGameList
+    const gameData = await fetchSingleGame(id);
     return setFetchedGame(gameData);
   }
+  async function fetchGallery(gameName) {
+    try {
+      const response = await fetch(`https://api.rawg.io/api/games/${gameName}/screenshots?key=c41ddde4984541a6aff7238cd9b5f310`);
+      const data = await response.json();
+      setGallery(data.results);
+    } 
+    catch (error){
+      return null;
+    }
+  }
   useEffect(()=>{ fetchGame() },[gameID]);
+  useEffect(()=>{ fetchedGame && fetchGallery(fetchedGame.slug) },[fetchedGame]);
+
+
   if (fetchedGame) {
     const { name,genres,metacritic,background_image,description,esrb_rating,developers } = fetchedGame;
     const descriptionToShow = parseDescriptionText(description);
     const genresToShow = genres ? genres.map(genre => genre.name).join(" - ") : "No data available";
     const developersToShow = developers ? developers.map( developer => developer.name).join(" - ") : "No data available";
     const esrbToShow = esrb_rating ? esrb_rating.name : "No data available";
+    const galleryToShow = gallery ? gallery.map( screen => {
+      return (
+        <img src={screen.image} key={screen.id} className="game-screenshot"/>
+      )
+    }) : <></>;
     return (
       <main className="game-main-wrapper">
         <h1 className="game-name">{name}</h1>
         <section className="game-title-wrapper">
-          <div>
+          <div className="game-title-data-wrapper">
             <h6 className="game-subtitle">METACRITIC SCORE</h6>
             <h5 className="game-subtitle-data">{metacritic}/100</h5>
             <h6 className="game-subtitle">GENRES</h6>
@@ -33,13 +54,16 @@ function Game( ) {
             <h6 className="game-subtitle">DEVELOPERS</h6>
             <h5 className="game-subtitle-data">{developersToShow}</h5>
           </div >
-          <img className="game-title-image" src={background_image} alt="background" />
+          <img className="game-title-image" src={background_image} alt={`${name} screenshot`} />
         </section>
         <section className="game-description-wrapper">
           <h6 className="game-subtitle">DESCRIPTION</h6>
           <p className="game-description">{descriptionToShow}</p>
           <br />
-          <button className="game-description-more" onClick={handleMoreClick}>SHOW MORE . . .</button>
+          <button className="game-description-more" onClick={handleMoreClick}>READ MORE</button>
+        </section>
+        <section className="game-gallery-wrapper">
+          {galleryToShow}
         </section>
       </main>
     )
--- src/services/handlers.js
+++ src/services/handlers.js
@@ -1,6 +1,7 @@
-function handleMoreClick() {
+function handleMoreClick(event) {
+  const buttonElement = event.target;
   const descriptionElement = document.querySelector(".game-description");
   descriptionElement.style.maxHeight === "fit-content" ? descriptionElement.style.maxHeight = "5.8rem" : descriptionElement.style.maxHeight = "fit-content";
-
+  descriptionElement.style.maxHeight === "fit-content" ? buttonElement.innerText = "HIDE" : buttonElement.innerText = "READ MORE";
 }
 export default handleMoreClick;
